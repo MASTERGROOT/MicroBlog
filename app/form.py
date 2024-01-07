28800f37cb8b2fb1ef81349db43e94c92f895024
Vivitthachai <vivitachaigood@hotmail.com>
2024-01-08 00:02:09 +0700

    add edit profile and avatar

--- app/form.py
+++ app/form.py
@@ -1,6 +1,6 @@
 ﻿from flask_wtf import FlaskForm
-from wtforms import StringField ,PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField ,PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -27,3 +27,8 @@ class Registration(FlaskForm):
         user_email = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user_email is not None:
             raise ValidationError('This email already exists')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
