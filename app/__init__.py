07d0a76ebe8a32b5494064b3928d30868eb799a0
Vivitthachai <vivitachaigood@hotmail.com>
2024-01-14 13:45:45 +0700

    add Email supported and reset password function (didn't connect to mailtrap yet)

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
 
 app = Flask(__name__)
+mail = Mail(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
